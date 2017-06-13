82eccf5f9fc20671d8084f47a2c78c42fa22dd01
TJ Rogers <trog0724@iastate.edu>
2017-06-13 13:51:19 -0500

    Parsing CSV files

---
+++ app/src/main/java/team_wallstreet/wallstreet/Utilities/StockParser.java
@@ -0,0 +1,28 @@
+package team_wallstreet.wallstreet.Utilities;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * Created by TJ on 6/13/2017.
+ */
+
+public class StockParser {
+
+    private HashMap<Date, >
+
+    public void StockParser(String stockCSV) {
+
+        Scanner scanner = new Scanner(stockCSV);
+
+        // Moves scanner past csv headers
+        scanner.nextLine();
+
+        while (scanner.hasNextLine()) {
+
+        }
+
+    }
+
+}
