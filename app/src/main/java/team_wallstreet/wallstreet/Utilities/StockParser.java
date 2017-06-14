5bbf5d05ffa2b0f5cad064da86a4b56efb684e62
TJ Rogers <trog0724@iastate.edu>
2017-06-14 14:34:57 -0500

    No longer using SQLite.

--- app/src/main/java/team_wallstreet/wallstreet/Utilities/StockParser.java
+++ app/src/main/java/team_wallstreet/wallstreet/Utilities/StockParser.java
@@ -12,9 +12,23 @@ import java.util.Scanner;
 
 public class StockParser {
 
-    private ArrayList<Date> date;
-    private double[] open, high, low, close, closeAdj;
-    private int[] volume;
+    private static final int SIZE_OF_HISTORY = 365;
+
+    String StockCode;
+    ArrayList<Date> DateList;
+    double[] OpenList, HighList, LowList, CloseList, CloseAdjList;
+    int[] VolumeList;
+
+    public void StockParser() {
+
+        DateList = new ArrayList();
+        OpenList = new double[SIZE_OF_HISTORY];
+        HighList = new double[SIZE_OF_HISTORY];
+        LowList = new double[SIZE_OF_HISTORY];
+        CloseList = new double[SIZE_OF_HISTORY];
+        CloseAdjList = new double [SIZE_OF_HISTORY];
+        VolumeList = new int[SIZE_OF_HISTORY];
+    }
 
     /**
      *  Parse CSV file into respective components and store them in a HashMap
@@ -22,13 +36,14 @@ public class StockParser {
      *  (Date (2012-05-21), double, double, double, double, double, int)
      *
      * @param stockCSV Csv file obtained from Yahoo Stocks.
+     * @param code stock code.
      */
-    public void StockParser(String stockCSV) {
+    public void StockParser(String stockCSV, String code) {
 
+        StockParser();
+        StockCode = code;
 
-
-
-        String entry;
+        int count = 0;
         Scanner scanner = new Scanner(stockCSV);
         Scanner tokenizer;
 
@@ -36,23 +51,65 @@ public class StockParser {
         scanner.nextLine();
 
         while (scanner.hasNextLine()) {
-            entry = scanner.nextLine();
-            tokenizer = new Scanner(entry);
-
-            // Change string to a Date and store in date.
-            try {
-                SimpleDateFormat df = new SimpleDateFormat("yyyy-mm-dd");
-                date.add(df.parse(tokenizer.next()));
-            } catch (ParseException e) {
-                e.printStackTrace();
-                date.add(null);
-            }
-
-
-
-            tokenizer.close();
+            parseStockString(scanner.nextLine(), count);
+            count++;
         }
+
         scanner.close();
     }
 
+    private void parseStockString(String data, int index) {
+
+        Scanner scanner = new Scanner(data);
+
+        // Change string to a Date and store in date.
+        try {
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-mm-dd");
+            DateList.add(df.parse(scanner.next()));
+        } catch (ParseException e) {
+            e.printStackTrace();
+            DateList.add(null);
+        }
+
+        OpenList[index] = scanner.nextDouble();
+        HighList[index] = scanner.nextDouble();
+        LowList[index] = scanner.nextDouble();
+        CloseList[index] = scanner.nextDouble();
+        CloseAdjList[index] = scanner.nextDouble();
+        VolumeList[index] = scanner.nextInt();
+
+        scanner.close();
+    }
+
+    public ArrayList<Date> getDateList() {
+        return DateList;
+    }
+
+    public double[] getOpenList() {
+        return OpenList;
+    }
+
+    public double[] getHighList() {
+        return HighList;
+    }
+
+    public double[] getLowList() {
+        return LowList;
+    }
+
+    public double[] getCloseList() {
+        return CloseList;
+    }
+
+    public double[] getCloseAdjList() {
+        return CloseAdjList;
+    }
+
+    public int[] getVolumeList() {
+        return VolumeList;
+    }
+
+    public String getStockCode() {
+        return StockCode;
+    }
 }
